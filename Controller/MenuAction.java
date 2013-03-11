85cf490125c068e3913a1e64eab0ba5508190b59
Li Hao Lai <lihao6485@hotmail.com>
2013-03-12 00:32:31 +0800

    - add new package (controller) - follow MCV to develop -- M model for XML/SVG handle -- C controller most for action listener and interactive between Model and View -- V GUI - basic SVG tag detection
    * file filter still have bug need to fix

---
+++ Controller/MenuAction.java
@@ -0,0 +1,61 @@
+package Controller;
+
+import GUI.DesktopPane;
+import Model.XMLReader;
+import java.io.File;
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFileChooser;
+import javax.swing.JInternalFrame;
+import javax.swing.JOptionPane;
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+public class MenuAction implements ActionListener {
+
+	public void actionPerformed(ActionEvent e) {
+		
+		if (e.getSource() == DesktopPane.openItem) {
+			
+			final JInternalFrame internal = new JInternalFrame("Open", true, true, true, true);
+			final JFileChooser fileChooser = new JFileChooser();
+			FileFilter fileFilter = new FileNameExtensionFilter("SVG files",".svg");
+			fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+			fileChooser.setFileFilter(fileFilter);
+			
+			internal.add(fileChooser, BorderLayout.CENTER);
+			internal.pack();
+			DesktopPane.desktopPane.add(internal);
+			internal.setVisible(true);
+			
+			fileChooser.addActionListener(
+					
+					new ActionListener() {
+						
+						public void actionPerformed(ActionEvent e) {
+							
+							if (e.getActionCommand().equals(JFileChooser.CANCEL_SELECTION)) {
+
+								internal.setVisible(false);
+								internal.dispose();
+							}
+							
+							File selectedFile = fileChooser.getSelectedFile();
+							
+							if ((selectedFile == null)||(selectedFile.getName().equals(""))) {
+								
+								//internal.setVisible(false);
+								internal.dispose();
+								JOptionPane.showInternalMessageDialog(internal, "Invalid Name", "Invalid Name", JOptionPane.ERROR_MESSAGE);
+							}
+							
+							internal.setVisible(false);
+							internal.dispose();
+							XMLReader reader = new XMLReader(selectedFile);
+						}
+					}
+			);
+		}
+	}
+}
--- GUI/DesktopPane.java
+++ GUI/DesktopPane.java
@@ -1,22 +1,17 @@
 package GUI;
 
-import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import Controller.MenuAction;
 import javax.swing.JFrame;
 import javax.swing.JDesktopPane;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
-import javax.swing.JInternalFrame;
-import javax.swing.JFileChooser;
-import javax.swing.filechooser.FileFilter;
-import javax.swing.filechooser.FileNameExtensionFilter;
 
 @SuppressWarnings("serial")
 public class DesktopPane extends JFrame {
 	
-	private JDesktopPane desktopPane;
+	public static JMenuItem openItem;
+	public static JDesktopPane desktopPane;
 	
 	public DesktopPane() {
 		
@@ -24,47 +19,14 @@ public class DesktopPane extends JFrame {
 		
 		JMenuBar menuBar = new JMenuBar();
 		JMenu fileMenu = new JMenu("File");
-		JMenuItem openItem = new JMenuItem("Open...");
+		openItem = new JMenuItem("Open...");
 		desktopPane = new JDesktopPane();
+		MenuAction action = new MenuAction();
 		
 		fileMenu.add(openItem);
 		menuBar.add(fileMenu);
 		setJMenuBar(menuBar);
 		add(desktopPane);
-		
-	 	openItem.addActionListener(
-	 	
-	 		new ActionListener() {
-	 		
-	 			public void actionPerformed(ActionEvent e) {
-	 				
-	 				final JInternalFrame internal = new JInternalFrame("Open", true, true, true, true);
-					JFileChooser fileChooser = new JFileChooser();
-					FileFilter fileFilter = new FileNameExtensionFilter("SVG files",".svg");
-					fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-					fileChooser.setFileFilter(fileFilter);
-					
-					internal.add(fileChooser, BorderLayout.CENTER);
-					internal.pack();
-					desktopPane.add(internal);
-					internal.setVisible(true);
-					
-					fileChooser.addActionListener(
-						
-						new ActionListener() {
-							
-							public void actionPerformed(ActionEvent e) {
-								
-								if (e.getActionCommand().equals(JFileChooser.CANCEL_SELECTION)) {
-
-									internal.setVisible(false);
-									internal.dispose();
-								}
-							}
-						}
-					);
-				}	
-	 		}
-		);
+		openItem.addActionListener(action);
 	}
 }
