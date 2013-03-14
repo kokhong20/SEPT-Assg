6bcaf4b647c51f7192b26c1d15c1470b3be53764
Li Hao Lai <lihao6485@hotmail.com>
2013-03-14 19:56:23 +0800

    - Retacangle class added

---
+++ Model/Rectangle.java
@@ -0,0 +1,57 @@
+package Model;
+
+import org.w3c.dom.Node;
+
+public class Rectangle extends Shapes
+{
+	private double x;
+	private double y;
+	private double width;
+	private double height;
+
+	public Rectangle(Node node) 
+	{
+		super(node);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void setX(double x)
+	{
+		this.x = x;
+	}
+	
+	public void setY(double y)
+	{
+		this.y = y;
+	}
+	
+	public void setWidth(double width)
+	{
+		this.width = width;
+	}
+	
+	public void setHeight(double height)
+	{
+		this.height = height;
+	}
+	
+	public double getX()
+	{
+		return this.x;
+	}
+	
+	public double getY()
+	{
+		return this.y;
+	}
+	
+	public double getWidth()
+	{
+		return this.width;
+	}
+	
+	public double getHeight()
+	{
+		return this.height;
+	}
+}
