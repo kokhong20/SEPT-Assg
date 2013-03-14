4c48523f1c8253b0553b9a97fea6087c6b6e376a
Li Hao Lai <lihao6485@hotmail.com>
2013-03-14 21:11:04 +0800

    - fix mainStart init problem

--- mainStart.java
+++ mainStart.java
@@ -1,9 +1,13 @@
 import GUI.DesktopPane;
+
 public class mainStart 
 {
 	public static void main(String args [])
 	{
-		new DesktopPane();
+		DesktopPane dp = new DesktopPane();
+		
+		dp.setVisible(true);
+		dp.setSize(600,600);
 	}
 
 }
