fc64976e87de1354a0b3870a63f43bce00fe6cee
Kok Hong <kokhong200@gmail.com>
2013-03-26 19:01:28 +0800

    Added InternalFrame class Added test svg files

---
+++ GUI/InternalFrame.java
@@ -0,0 +1,52 @@
+package GUI;
+
+import javax.swing.JDesktopPane;
+import javax.swing.JInternalFrame;
+
+import Controller.InternalFrameAction;
+
+public class InternalFrame extends JInternalFrame 
+{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4520525531144579091L;
+	
+	JDesktopPane desktopPane;
+	SVGRender svgPanel;
+	
+	/* Internal Frame Item */
+	JInternalFrame internalFrame;
+	
+	
+	public InternalFrame(JDesktopPane destopPane , String frameTitle)
+	{
+		this.desktopPane = desktopPane;
+		initInternalFrame(frameTitle);
+	}
+	
+	public InternalFrame(JDesktopPane desktopPan, SVGRender svgPanel , String frameTitle)
+	{
+		this.desktopPane = desktopPane;
+		this.svgPanel = svgPanel;
+		initInternalFrame(frameTitle);
+		this.addActionListener();
+	}
+	
+	private void initInternalFrame(String frameTitle)
+	{
+		internalFrame = new JInternalFrame(frameTitle,true,true,true,true);
+		desktopPane.add(internalFrame);
+	}
+	
+	private void addActionListener()
+	{
+		InternalFrameAction svgFrameAction= new InternalFrameAction(this,svgPanel);
+		this.addInternalFrameListener(svgFrameAction);
+	}
+	
+	
+	
+
+}
