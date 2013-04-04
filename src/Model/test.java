bfbfa2aa32a7f528b948528ea562100f0524cc24
Li Hao Lai <lihao6485@hotmail.com>
2013-04-04 22:06:19 +0800

    fresh start for v2

---
+++ src/Model/test.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author LiHao
+ */
+public class test
+{
+    public static void main (String[]args)
+    {
+        System.out.println("test");
+    }
+}
