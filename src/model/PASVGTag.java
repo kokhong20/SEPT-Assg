7955ca7cfb01de3ec7547be9e1046e9a6a9ed679
Lai Sai Hoo <laisaihoo@gmail.com>
2013-04-18 18:38:46 +0800

    MUST READ
    PACircle
    PALine
    PARectangle
    PAShape
    
    parmeter of setUnit in PAUnit changed to (String, boolean, double) where
    the double is the size for calculate if it is percentage (put 0 if
    percentage is not applicable there or use setSymbol)

--- src/model/PASVGTag.java
+++ src/model/PASVGTag.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package model;
 
 import java.awt.Color;
@@ -320,3 +321,383 @@ public class PASVGTag implements PAAttributeConstant
     }
 
 }
+=======
+package model;
+
+import java.awt.Color;
+import java.util.regex.Pattern;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+
+/**
+ * 
+ * @author LaiSaiHoo
+ * 
+ */
+public class PASVGTag
+{
+	private double width;
+	private double height;
+	private double initX;
+	private double initY;
+	private double viewHeight;
+	private double viewWidth;
+	private double scaleX;
+	private double scaleY;
+	private Color stroke;
+	private Color fill;
+	private double strokeWidth;
+	private Node node;
+	private double scale;
+
+	/**
+	 * Constructor which receives a node
+	 * 
+	 * @param node
+	 */
+	public PASVGTag(Node node)
+	{
+		setNode(node);
+		setSVGAttr();
+	}
+
+	/**
+	 * Set values to variables to current instance if an Element Node
+	 */
+	private void setSVGAttr()
+	{
+		if (this.getNode().getNodeType() == Node.ELEMENT_NODE)
+		{
+			Element eNode = (Element) this.getNode();
+
+			// if (eNode.hasAttribute("width") == true)
+			// {
+			// if (eNode.getAttribute("width").matches("\\d+%{1}"))
+			// {
+			// setWidth(PAUnit.setPercentage(eNode.getAttribute("width"),
+			// 500));
+			// }
+			// else
+			// {
+			// setWidth(PAUnit.setUnit(eNode.getAttribute("width"), false));
+			// }
+			// }
+			// else
+			// {
+			// setWidth(500);
+			// }
+			//
+			// if (eNode.hasAttribute("height") == true)
+			// {
+			// if (eNode.getAttribute("height").matches("\\d+%{1}"))
+			// {
+			// setHeight(PAUnit.setPercentage(
+			// eNode.getAttribute("height"), 500));
+			// }
+			// else
+			// {
+			// setHeight(PAUnit.setUnit(eNode.getAttribute("height"),
+			// false));
+			// }
+			// }
+			// else
+			// {
+			// setHeight(500);
+			// }
+
+			setWidth(eNode.hasAttribute("width") ? PAUnit.setUnit(
+					eNode.getAttribute("width"), false, 500) : 500);
+			setHeight(eNode.hasAttribute("height") ? PAUnit.setUnit(
+					eNode.getAttribute("height"), false, 500) : 500);
+
+			if (eNode.hasAttribute("viewBox"))
+			{
+				setViewbox(eNode.getAttribute("viewBox"));
+			}
+			else
+			{
+				setInitX(0);
+				setInitY(0);
+				setViewWidth(0);
+				setViewHeight(0);
+			}
+
+			setScale(this.width, this.height, this.viewWidth, this.viewHeight);
+			setScaleXY(getScaleX(), getScaleY());
+			setStrokeWidth(PAUnit.setUnit(eNode.getAttribute("stroke-width"),
+					true, 0));
+			setStroke(PAColor.setColor(eNode.getAttribute("stroke"), true));
+			setFill(PAColor.setColor(eNode.getAttribute("fill"), false));
+		}
+	}
+
+	/**
+	 * Read in value as value of svg viewbox attribute set it to current
+	 * instance variables
+	 * 
+	 * @param value
+	 */
+	public void setViewbox(String value)
+	{
+		if (Pattern.matches("\\d+ *,* *\\d+ *,* *\\d+ *,* *\\d+", value))
+		{
+			String values[] = value.split("[ *,* *]+");
+			setInitX(PAUnit.setUnit(values[0], false, 0));
+			setInitY(PAUnit.setUnit(values[1], false, 0));
+			setViewWidth(PAUnit.setUnit(values[2], false, 0));
+			setViewHeight(PAUnit.setUnit(values[3], false, 0));
+		}
+		else
+		{
+			setInitX(0);
+			setInitY(0);
+			setViewWidth(0);
+			setViewHeight(0);
+		}
+	}
+
+	/**
+	 * @return the width
+	 */
+	public double getWidth()
+	{
+		return width;
+	}
+
+	/**
+	 * @param width
+	 *            the width to set
+	 */
+	public void setWidth(double width)
+	{
+		this.width = width;
+	}
+
+	/**
+	 * @return the height
+	 */
+	public double getHeight()
+	{
+		return height;
+	}
+
+	/**
+	 * @param height
+	 *            the height to set
+	 */
+	public void setHeight(double height)
+	{
+		this.height = height;
+	}
+
+	/**
+	 * @return the initX
+	 */
+	public double getInitX()
+	{
+		return initX;
+	}
+
+	/**
+	 * @param initX
+	 *            the initX to set
+	 */
+	public void setInitX(double initX)
+	{
+		this.initX = initX;
+	}
+
+	/**
+	 * @return the initY
+	 */
+	public double getInitY()
+	{
+		return initY;
+	}
+
+	/**
+	 * @param initY
+	 *            the initY to set
+	 */
+	public void setInitY(double initY)
+	{
+		this.initY = initY;
+	}
+
+	/**
+	 * @return the viewHeight
+	 */
+	public double getViewHeight()
+	{
+		return viewHeight;
+	}
+
+	/**
+	 * @param viewHeight
+	 *            the viewHeight to set
+	 */
+	public void setViewHeight(double viewHeight)
+	{
+		this.viewHeight = viewHeight;
+	}
+
+	/**
+	 * @return the viewWidth
+	 */
+	public double getViewWidth()
+	{
+		return viewWidth;
+	}
+
+	/**
+	 * @param viewWidth
+	 *            the viewWidth to set
+	 */
+	public void setViewWidth(double viewWidth)
+	{
+		this.viewWidth = viewWidth;
+	}
+
+	/**
+	 * @return the stroke
+	 */
+	public Color getStroke()
+	{
+		return stroke;
+	}
+
+	/**
+	 * @param stroke
+	 *            the stroke to set
+	 */
+	public void setStroke(Color stroke)
+	{
+		this.stroke = stroke;
+	}
+
+	/**
+	 * @return the fill
+	 */
+	public Color getFill()
+	{
+		return fill;
+	}
+
+	/**
+	 * @param fill
+	 *            the fill to set
+	 */
+	public void setFill(Color fill)
+	{
+		this.fill = fill;
+	}
+
+	/**
+	 * @return the strokeWidth
+	 */
+	public double getStrokeWidth()
+	{
+		return strokeWidth;
+	}
+
+	/**
+	 * @param strokeWidth
+	 *            the strokeWidth to set
+	 */
+	public void setStrokeWidth(double strokeWidth)
+	{
+		this.strokeWidth = strokeWidth;
+	}
+
+	/**
+	 * @return the node
+	 */
+	public Node getNode()
+	{
+		return node;
+	}
+
+	/**
+	 * @param node
+	 *            the node to set
+	 */
+	private void setNode(Node node)
+	{
+		this.node = node;
+	}
+
+	/**
+	 * @return the scaleX
+	 */
+	public double getScaleX()
+	{
+		return scaleX;
+	}
+
+	/**
+	 * @param scaleX
+	 *            the scaleX to set
+	 */
+	public void setScaleX(double scaleX)
+	{
+		this.scaleX = scaleX;
+	}
+
+	/**
+	 * @return the scaleY
+	 */
+	public double getScaleY()
+	{
+		return scaleY;
+	}
+
+	/**
+	 * @param scaleY
+	 *            the scaleY to set
+	 */
+	public void setScaleY(double scaleY)
+	{
+		this.scaleY = scaleY;
+	}
+
+	/**
+	 * 
+	 * @param oriWidth
+	 * @param oriHeight
+	 * @param viewWidth
+	 * @param viewHeight
+	 */
+	public void setScale(double oriWidth, double oriHeight, double viewWidth,
+			double viewHeight)
+	{
+		if (this.viewWidth == 0 && this.viewHeight == 0)
+		{
+			this.setScaleX(1);
+			this.setScaleY(1);
+		}
+		else
+		{
+			this.setScaleX((oriWidth / viewWidth));
+			this.setScaleY((oriHeight / viewHeight));
+		}
+	}
+
+	public double getScaleXY()
+	{
+		return scale;
+	}
+
+	public void setScaleXY(double scaleX, double scaleY)
+	{
+		if (scaleX > scaleY)
+		{
+			this.scale = scaleY;
+		}
+		else
+		{
+			this.scale = scaleX;
+		}
+	}
+
+}
+>>>>>>> PAShape
--- src/model/PAUnit.java
+++ src/model/PAUnit.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package model;
 
 import java.util.regex.Pattern;
@@ -213,3 +214,245 @@ public class PAUnit implements PAAttributeConstant
 	}
 
 }
+=======
+package model;
+
+import java.util.regex.Pattern;
+
+/**
+ * @author bryantylai/SaiHoo
+ * 
+ */
+
+public class PAUnit
+{
+	/**
+	 * create a static variable as this is constantly used across the class
+	 * 
+	 */
+	public static double dpi = PASystem.getDotsPerInch();
+
+	public static double setUnit(String att, boolean isStrokeWidth, double size)
+	{
+		if (att.matches("\\d+%{1}"))
+		{
+			return setPercentage(att, size);
+		}
+		else
+		{
+			return setSymbol(att, isStrokeWidth);
+		}
+	}
+	
+	
+	/**
+	 * removed convert the string to lower case as svg is case sensitive
+	 * 
+	 * Validate and convert the SVG element attributes value received as String
+	 * to double
+	 * 
+	 * @param att
+	 *            attribute value of an SVG element attribute
+	 * @param isStrokeWidth
+	 *            true if attribute value passed in is from stroke-width
+	 * @return unit of length in double
+	 */
+
+	public static double setSymbol(String att, boolean isStrokeWidth)
+	{
+		if (!att.isEmpty())
+		{
+			// 10.10px or 10px or .10px are valid
+			if (Pattern
+					.matches(
+							"(\\-?\\d+\\.?\\d+[e]?\\d*)+(em|ex|px|in|cm|mm|pt|pc)",
+							att)
+					|| Pattern
+							.matches(
+									"(\\-?\\d+[e]?\\d*)+(em|ex|px|in|cm|mm|pt|pc)",
+									att)
+					|| Pattern.matches(
+							"(\\-?\\.?\\d+[e]?\\d*)+(em|ex|px|in|cm|mm|pt|pc)",
+							att))
+			{
+				switch (att.substring(att.length() - 2))
+				{
+				case "em":
+					return convertEM(calculate(removeUnits(att)));
+				case "ex":
+					return convertEX(calculate(removeUnits(att)));
+				case "px":
+					return convertPX(calculate(removeUnits(att)));
+				case "in":
+					return convertIN(calculate(removeUnits(att)));
+				case "cm":
+					return convertCM(calculate(removeUnits(att)));
+				case "mm":
+					return convertMM(calculate(removeUnits(att)));
+				case "pt":
+					return convertPT(calculate(removeUnits(att)));
+				case "pc":
+					return convertPC(calculate(removeUnits(att)));
+				}
+			}
+			// 10 or 10.10 or .10 are valid
+			else if (Pattern.matches("(\\-?\\d+[e]?\\d*)", att)
+					|| Pattern.matches("(\\-?\\d+.\\d+[e]?\\d*)", att)
+					|| Pattern.matches("(\\-?\\.?\\d+[e]?\\d*)", att))
+			{
+				return Double.parseDouble(calculate(att));
+			}
+		}
+
+		if (isStrokeWidth)
+			return 1;
+		else
+			return 0;
+	}
+
+	/**
+	 * To convert percentage to size
+	 * 
+	 * @param att
+	 * @param size
+	 * @return calculated size with respect to size
+	 */
+	public static double setPercentage(String att, double size)
+	{
+		double percent = Double.parseDouble(att.replace(
+				att.substring(att.length() - 1), "")) / 100;
+		return percent * size;
+	}
+
+	/**
+	 * To calculate exponential values
+	 * 
+	 * @param att
+	 * @return a String with its exponential value calculated
+	 */
+	private static String calculate(String att)
+	{
+		if (att.contains("e"))
+		{
+			int loop = Integer.parseInt(att.substring(att.indexOf("e") + 1));
+			double value = Double
+					.parseDouble(att.substring(0, att.indexOf("e")));
+
+			value = value * Math.pow(10, loop);
+
+			return String.valueOf(value);
+		}
+
+		return att;
+	}
+
+	/**
+	 * remove symbol from a attribute value
+	 * 
+	 * @param att
+	 *            attribute value
+	 * @return a String with its symbol removed
+	 */
+	private static String removeUnits(String att)
+	{
+		return att.replace(att.substring(att.length() - 2), "");
+	}
+
+	/**
+	 * Convert from EM to PX
+	 * 
+	 * @param value
+	 *            attribute value
+	 * @return length of attribute value double converted from EM
+	 */
+	public final static double convertEM(String value)
+	{
+		return (Double.parseDouble(value) * 12 * dpi / 72);
+	}
+
+	/**
+	 * Convert from EX to PX
+	 * 
+	 * @param value
+	 *            attribute value
+	 * @return length of attribute value double converted from EX
+	 */
+	public final static double convertEX(String value)
+	{
+		return Double.parseDouble(value) * 7;
+	}
+
+	/**
+	 * Convert from PX to PX
+	 * 
+	 * @param value
+	 *            attribute value
+	 * @return length of attribute value double converted from PX
+	 */
+	public final static double convertPX(String value)
+	{
+		return Double.parseDouble(value);
+	}
+
+	/**
+	 * Convert from PT to PX
+	 * 
+	 * @param value
+	 *            attribute value
+	 * @return length of attribute value double converted from PT
+	 */
+	public final static double convertPT(String value)
+	{
+		return (Double.parseDouble(value) * dpi / 72);
+	}
+
+	/**
+	 * Convert from PC to PX
+	 * 
+	 * @param value
+	 *            attribute value
+	 * @return length of attribute value double converted from PC
+	 */
+	public final static double convertPC(String value)
+	{
+		return (Double.parseDouble(value) * dpi / 6);
+	}
+
+	/**
+	 * Convert from MM to PX
+	 * 
+	 * @param value
+	 *            attribute value
+	 * @return length of attribute value double converted from MM
+	 */
+	public final static double convertMM(String value)
+	{
+		return (Double.parseDouble(value) * dpi / 25.4);
+	}
+
+	/**
+	 * Convert from CM to PX
+	 * 
+	 * @param value
+	 *            attribute value
+	 * @return length of attribute value double converted from CM
+	 */
+	public final static double convertCM(String value)
+	{
+		return (Double.parseDouble(value) * dpi / 2.54);
+	}
+
+	/**
+	 * Convert from IN to PX
+	 * 
+	 * @param value
+	 *            attribute value
+	 * @return length of attribute value double converted from IN
+	 */
+	public final static double convertIN(String value)
+	{
+		return Double.parseDouble(value) * dpi;
+	}
+
+}
+>>>>>>> PAShape
