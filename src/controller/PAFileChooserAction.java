6b64f7f11b9095a335235c7c9d06973ccfc8a685
Li Hao Lai <lihao6485@hotmail.com>
2013-04-20 00:41:17 +0800

    - removed unused comment - indent

--- src/controller/PAFileChooserAction.java
+++ src/controller/PAFileChooserAction.java
@@ -14,65 +14,54 @@ import model.PASVGContainer;
 import model.PASVGImport;
 import model.PASVGTag;
 import model.PAShape;
-import model.PASystem;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 
 /**
- * 
+ *
  * @author LiHao
  */
 public class PAFileChooserAction implements ActionListener
 {
-	private JFileChooser fileChooser;
-	private JInternalFrame frame;
-	private PASVGImport svgImport;
+    private JFileChooser fileChooser;
+    private JInternalFrame frame;
+    private PASVGImport svgImport;
 
-	/**
-	 * 
-	 * Contructor for file chooser's action.
-	 * 
-	 * @param fileChooser
-	 *            pass existing file chooser.
-	 * @param frame
-	 *            internal frame which store file chooser.
-	 */
-	public PAFileChooserAction(JFileChooser fileChooser, JInternalFrame frame)
-	{
-		this.fileChooser = fileChooser;
-		this.frame = frame;
-	}
+    /**
+     *
+     * Contructor for file chooser's action.
+     *
+     * @param fileChooser pass existing file chooser.
+     * @param frame internal frame which store file chooser.
+     */
+    public PAFileChooserAction(JFileChooser fileChooser, JInternalFrame frame)
+    {
+        this.fileChooser = fileChooser;
+        this.frame = frame;
+    }
 
-	/**
-	 * action in file chooser (Cancel and Open button)
-	 * 
-	 * @param e
-	 */
-	@Override
-	public void actionPerformed(ActionEvent e)
-	{
-		// Open Button Action
-		if (!e.getActionCommand().equals(JFileChooser.CANCEL_SELECTION))
-		{
-			File selectedFile = fileChooser.getSelectedFile();
-			// String path = fileChooser.getCurrentDirectory().toString() ;
-			// String completedPath = PASystem.setPath(path, selectedFile);
-			 Document svgDoc = PASVGImport.processFiletoDoc(selectedFile);
-			 LinkedList<PAShape> shapesCollection =
-			 PASVGImport.readSVGElements(svgDoc);
-//			svgImport = new PASVGImport(selectedFile);
-//			Document svgDoc = svgImport.getSVGDoc();
-//			LinkedList<PAShape> shapesColection = svgImport
-//					.getShapeCollection();
-			Node svgNode = svgDoc.getElementsByTagName("svg").item(0);
-			PASVGTag svgTag = new PASVGTag(svgNode);
-			// PASVGContainer svgContainer = new PASVGContainer(svgTag,
-			// shapesCollection);
-		}
+    /**
+     * action in file chooser (Cancel and Open button)
+     *
+     * @param e
+     */
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        // Open Button Action
+        if (!e.getActionCommand().equals(JFileChooser.CANCEL_SELECTION))
+        {
+            File selectedFile = fileChooser.getSelectedFile();
+            Document svgDoc = PASVGImport.processFiletoDoc(selectedFile);
+            LinkedList<PAShape> shapesCollection = PASVGImport.readSVGElements(svgDoc);
+            Node svgNode = svgDoc.getElementsByTagName("svg").item(0);
+            PASVGTag svgTag = new PASVGTag(svgNode);
+            PASVGContainer svgContainer = new PASVGContainer(svgTag, shapesCollection);
+        }
 
-		// need to do for both Cancel and Open button.
-		frame.setVisible(false);
-		frame.dispose();
-	}
+        // need to do for both Cancel and Open button.
+        frame.setVisible(false);
+        frame.dispose();
+    }
 
 }
--- src/model/PASVGImport.java
+++ src/model/PASVGImport.java
@@ -12,120 +12,106 @@ import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 /**
- * 
- * @author bryantylai/LiHao
+ *
+ * @author bryantylai/LiHao／Lai Sai Hoo
  */
 public class PASVGImport
 {
-	/**
-	 * 
-	 * Process current SVG file by parsing the SVG file to be readable in JAVA
-	 * 
-	 * @param svgFile
-	 *            file from file chooser
-	 * @return svg's Document.
-	 */
-	public static Document processFiletoDoc(File svgFile)
-	{
-		Document svgDoc = null;
-		DocumentBuilderFactory svgDBFactory = DocumentBuilderFactory
-				.newInstance();
+    /**
+     *
+     * Process current SVG file by parsing the SVG file to be readable in JAVA
+     *
+     * @param svgFile file from file chooser
+     * @return svg's Document.
+     */
+    public static Document processFiletoDoc(File svgFile)
+    {
+        Document svgDoc = null;
+        DocumentBuilderFactory svgDBFactory = DocumentBuilderFactory.newInstance();
 
-		try
-		{
-			svgDBFactory.setFeature("http://xml.org/sax/features/namespaces",
-					false);
-			svgDBFactory.setFeature("http://xml.org/sax/features/validation",
-					false);
-			svgDBFactory
-					.setFeature(
-							"http://apache.org/xml/features/nonvalidating/load-dtd-grammar",
-							false);
-			svgDBFactory
-					.setFeature(
-							"http://apache.org/xml/features/nonvalidating/load-external-dtd",
-							false);
+        try
+        {
+            svgDBFactory.setFeature("http://xml.org/sax/features/namespaces", false);
+            svgDBFactory.setFeature("http://xml.org/sax/features/validation", false);
+            svgDBFactory.setFeature("http://apache.org/xml/features/nonvalidating/load-dtd-grammar", false);
+            svgDBFactory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+            
+            DocumentBuilder svgDocBuilder = svgDBFactory.newDocumentBuilder();
+            svgDoc = svgDocBuilder.parse(svgFile);
+            svgDoc.getDocumentElement().normalize();
+        }
+        catch (ParserConfigurationException | SAXException | IOException ex)
+        {
+            System.err.println(ex.getMessage());
+        }
 
-			DocumentBuilder svgDocBuilder = svgDBFactory.newDocumentBuilder();
-			svgDoc = svgDocBuilder.parse(svgFile);
-			svgDoc.getDocumentElement().normalize();
-		}
-		catch (ParserConfigurationException | SAXException | IOException ex)
-		{
-			System.err.println(ex.getMessage());
-		}
+        return svgDoc;
+    }
 
-		return svgDoc;
-	}
+    /**
+     * Read the current SVG file and add all available elements (rect, circle,
+     * line, g) into a LinkedList
+     *
+     * @return a LinkedList of all shapes
+     */
+    public static LinkedList<PAShape> readSVGElements(Document svgDoc)
+    {
 
-	/**
-	 * Read the current SVG file and add all available elements (rect, circle,
-	 * line, g) into a LinkedList
-	 * 
-	 * @return a LinkedList of all shapes
-	 */
-	public static LinkedList<PAShape> readSVGElements(Document svgDoc)
-	{
+        LinkedList<PAShape> shapesCollection = new LinkedList<>();
+        // TODO Auto-generated method stub
+        if (svgDoc != null)
+        {
+            // Processing SVG tag
+            Node svg = svgDoc.getElementsByTagName("svg").item(0);
+            NodeList drawList = svg.getChildNodes();
+            
+            for (int index = 0; index < drawList.getLength(); index++)
+            {
+                switch (drawList.item(index).getNodeName())
+                {
+                    case "rect":
+                        PARectangle newRect = new PARectangle(drawList.item(index));
+                        newRect.readAttributes();
 
-		LinkedList<PAShape> shapesCollection = new LinkedList<PAShape>();
-		// TODO Auto-generated method stub
-		if (svgDoc != null)
-		{
-			// Processing SVG tag
-			Node svg = svgDoc.getElementsByTagName("svg").item(0);
-			System.out.println(svg.DOCUMENT_POSITION_CONTAINS);
+                        // Add new rectangle object to Shapes and put into linked
+                        // list
+                        // Rectangle2D.Double rectShape = new
+                        // Rectangle2D.Double(newRect.getX(),newRect.getY(),newRect.getWidth(),newRect.getHeight());
+                        shapesCollection.add(newRect);
+                        break;
 
-			NodeList drawList = svg.getChildNodes();
-			for (int index = 0; index < drawList.getLength(); index++)
-			{
-				switch (drawList.item(index).getNodeName())
-				{
-				case "rect":
-					PARectangle newRect = new PARectangle(drawList.item(index));
-					newRect.readAttributes();
+                    case "circle":
+                        PACircle newCircle = new PACircle(drawList.item(index));
+                        newCircle.readAttributes();
 
-					// Add new rectangle object to Shapes and put into linked
-					// list
-					// Rectangle2D.Double rectShape = new
-					// Rectangle2D.Double(newRect.getX(),newRect.getY(),newRect.getWidth(),newRect.getHeight());
-					shapesCollection.add(newRect);
-					break;
+                        // Add new circle object to Shapes and put into linked List
+                        // Ellipse2D.Double circleShape = new
+                        // Ellipse2D.Double(newCircle.getEllipse2DX(),newCircle.getEllipse2DY(),newCircle.getR()*2,newCircle.getR()*2);
 
-				case "circle":
-					PACircle newCircle = new PACircle(drawList.item(index));
-					newCircle.readAttributes();
+                        shapesCollection.add(newCircle);
+                        break;
 
-					// Add new circle object to Shapes and put into linked List
-					// Ellipse2D.Double circleShape = new
-					// Ellipse2D.Double(newCircle.getEllipse2DX(),newCircle.getEllipse2DY(),newCircle.getR()*2,newCircle.getR()*2);
+                    case "line":
+                        PALine newLine = new PALine(drawList.item(index));
+                        newLine.readAttributes();
 
-					shapesCollection.add(newCircle);
-					break;
+                        shapesCollection.add(newLine);
+                        break;
 
-				case "line":
-					PALine newLine = new PALine(drawList.item(index));
-					newLine.readAttributes();
+                    // case "g":
+                    // NodeList gList = drawList.item(index).getChildNodes();
+                    // createGroups(this.shapesCollection, gList,
+                    // drawList.item(index), null, null);
+                    //
+                    // break;
+                }
+            }
+        }
 
-					shapesCollection.add(newLine);
-					break;
+        /**
+         * Reading of svg elements not done yet
+         */
+        return shapesCollection;
+    }
 
-				// case "g":
-				// NodeList gList = drawList.item(index).getChildNodes();
-				// createGroups(this.shapesCollection, gList,
-				// drawList.item(index), null, null);
-				//
-				// break;
-				}
-			}
-		}
-		else if (svgDoc == null)
-		{
-			System.out.println("let me know");
-		}
-
-		/**
-		 * Reading of svg elements not done yet
-		 */
-		return shapesCollection;
-	}
 }
