c24a75dc22b8d50e5d95ddd555c3063107b64d40
Li Hao Lai <lihao6485@hotmail.com>
2013-04-25 19:38:51 +0800

    - draw circle

--- src/gui/PASVGPanel.java
+++ src/gui/PASVGPanel.java
@@ -1,10 +1,20 @@
 package gui;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Dimension;
-
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+import java.util.Iterator;
+import java.util.LinkedList;
 import javax.swing.JPanel;
+import model.PACircle;
 import model.PASVGContainer;
+import model.PASVGElement;
 
 /**
  *
@@ -13,17 +23,30 @@ import model.PASVGContainer;
  */
 public class PASVGPanel extends JPanel
 {
+    private int svgWidth;
+    private int svgHeight;
+    private double zoomScale;
+    private double xPosition;
+    private double yPosition;
+    private BufferedImage svgImage;
     private PAMainFrame mainFrame;
     private PASVGContainer svgContainer;
+    private LinkedList<PASVGElement> elementCollection;
 
     /**
      * constructor to define PASVGPanel for PAMainFrame
+     *
      * @param mainFrame
      */
     public PASVGPanel(PASVGContainer svgContainer)
     {
         this.svgContainer = svgContainer;
+        this.elementCollection = svgContainer.getSvgContainer();
+        zoomScale = 1;
+	xPosition = 0;
+	yPosition = 0;
         initPanel();
+        drawToImage();
     }
 
     /**
@@ -31,14 +54,84 @@ public class PASVGPanel extends JPanel
      */
     private void initPanel()
     {
-        int width = (int)svgContainer.getSvgTag().getWidth();
-        int height = (int)svgContainer.getSvgTag().getHeight();
-        Dimension size = new Dimension(width, height);
+        svgWidth = (int) svgContainer.getSvgTag().getWidth();
+        svgHeight = (int) svgContainer.getSvgTag().getHeight();
+        Dimension size = new Dimension(svgWidth, svgHeight);
 
         setBackground(Color.white);
         setMaximumSize(size);
         setMinimumSize(size);
         setPreferredSize(size);
+    }
+
+    private void drawToImage()
+    {
+        svgImage = new BufferedImage(svgWidth, svgHeight, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2d = svgImage.createGraphics();
+
+        //for anti-aliasing for better output.
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2d.fillRect(0, 0, svgWidth, svgHeight);
+        g2d.setPaint(new Color(255,255,255,255));
+        
+        if (!elementCollection.isEmpty())
+        {
+            float strokeWidth;
+            double x, y, diameter, width, height;
+            BasicStroke stroke;
+            Iterator<PASVGElement> it = elementCollection.iterator();
+
+            while (it.hasNext())
+            {
+                PASVGElement drawItem = it.next();
+                if (drawItem instanceof PACircle)
+                {
+                    x = ((PACircle)drawItem).getCx() - ((PACircle)drawItem).getR() * zoomScale + xPosition;
+                    y = ((PACircle)drawItem).getCy() - ((PACircle)drawItem).getR() * zoomScale + yPosition;
+                    diameter = ((PACircle)drawItem).getR() * 2 * zoomScale;
+                    strokeWidth = (float) ((PASVGElement)drawItem).getStrokeWidth();
+                    stroke = new BasicStroke(strokeWidth * (float) zoomScale);
+                    
+                    // creating 2D Shapes object 
+                    Ellipse2D.Double circle = new Ellipse2D.Double(x, y, diameter, diameter);
+                    g2d.setColor(((PASVGElement)drawItem).getFill());
+                    g2d.fill(circle);
+                    g2d.setColor(((PASVGElement)drawItem).getStroke());
+                    g2d.setStroke(stroke);
+                    g2d.draw(circle);
+                }
+                /*else if (drawItem instanceof Rectangles)
+                {
+                    // creating 2D Shapes object 
+                    Rectangle2D.Double rectShape = new Rectangle2D.Double(((Rectangles) drawItem).getX() * zoomScale + xPosition,
+                            ((Rectangles) drawItem).getY() * zoomScale + yPosition, ((Rectangles) drawItem).getWidth() * zoomScale, ((Rectangles) drawItem).getHeight() * zoomScale);
+
+                    g2d.setColor(((Shapes) drawItem).getFill());
+                    g2d.fill(rectShape);
+                    g2d.setColor(((Drawings) drawItem).getStrokeColor());
+                    g2d.setStroke(new BasicStroke(((Drawings) drawItem).getStrokeWidth() * ((float) zoomScale)));
+                    g2d.draw(rectShape);
+                }
+                else if (drawItem instanceof Lines)
+                {
+                    // creating 2D Shapes object 
+
+                    Line2D.Double lineShape = new Line2D.Double(((Lines) drawItem).getX1() * zoomScale + xPosition,
+                            ((Lines) drawItem).getY1() * zoomScale + yPosition, ((Lines) drawItem).getX2() * zoomScale + xPosition,
+                            ((Lines) drawItem).getY2() * zoomScale + yPosition);
+
+                    g2d.setColor(((Drawings) drawItem).getStrokeColor());
+                    g2d.setStroke(new BasicStroke(((Drawings) drawItem).getStrokeWidth() * ((float) zoomScale)));
+                    g2d.draw(lineShape);
+                }*/
+            }
+        }
+    }
+
+    @Override
+    public void paintComponent(Graphics g)
+    {
+        g.drawImage(svgImage, 0, 0, this);
     }
 
 }
--- src/model/PACircle.java
+++ src/model/PACircle.java
@@ -5,101 +5,109 @@ import java.awt.Color;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 
-public class PACircle extends PASVGElement {
+public class PACircle extends PASVGElement
+{
+    private double cx;
+    private double cy;
+    private double r;
 
-	private double cx;
-	private double cy;
-	private double r;
+    /**
+     *
+     */
+    public PACircle()
+    {
+    }
 
-        /**
-         * 
-         */
-	public PACircle()
-	{
-		
-	}
-	
-	/**
-	 * Constructor which receives a node
-	 * 
-	 * @param node
-	 */
-	public PACircle(Node node) 
-	{
-		super(node);
-	}
+    /**
+     * Constructor which receives a node
+     *
+     * @param node
+     */
+    public PACircle(Node node)
+    {
+        super(node);
+    }
 
-        /**
-	 * Constructor which receives a node and groupFill, groupStroke, groupWidth from parent group
-         * @param node
-         * @param groupFill
-         * @param groupStroke
-         * @param groupWidth
-         * @param parentGroup 
-         */
-	public PACircle(Node node, Color groupFill, Color groupStroke,
-			double groupWidth, PASVGGroup parentGroup)
-	{
-		// TODO Auto-generated constructor stub
-		super(node, groupFill, groupStroke, groupWidth, parentGroup);
-	}
+    /**
+     * Constructor which receives a node and groupFill, groupStroke, groupWidth
+     * from parent group
+     *
+     * @param node
+     * @param groupFill
+     * @param groupStroke
+     * @param groupWidth
+     * @param parentGroup
+     */
+    public PACircle(Node node, Color groupFill, Color groupStroke,
+            double groupWidth, PASVGGroup parentGroup)
+    {
+        // TODO Auto-generated constructor stub
+        super(node, groupFill, groupStroke, groupWidth, parentGroup);
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public void readAttributes()
-	{
-		// TODO Auto-generated method stub
-		if (this.getNode().getNodeType() == Node.ELEMENT_NODE)
-		{
-			Element eNode = (Element) this.getNode();
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void readAttributes()
+    {
+        // TODO Auto-generated method stub
+        if (this.getNode().getNodeType() == Node.ELEMENT_NODE)
+        {
+            Element eNode = (Element) this.getNode();
 
-			this.setCx(PAUnit.setUnit(eNode.getAttribute("cx"), DEFAULT_LENGTH));
-			this.setCy(PAUnit.setUnit(eNode.getAttribute("cy"), DEFAULT_LENGTH));
-			this.setR(PAUnit.setUnit(eNode.getAttribute("r"), DEFAULT_LENGTH));
-		}
-	}
+            this.setCx(PAUnit.setUnit(eNode.getAttribute("cx"), DEFAULT_LENGTH));
+            this.setCy(PAUnit.setUnit(eNode.getAttribute("cy"), DEFAULT_LENGTH));
+            this.setR(PAUnit.setUnit(eNode.getAttribute("r"), DEFAULT_LENGTH));
+        }
+    }
 
-	/**
-	 * @return the cx
-	 */
-	public double getCx() {
-		return cx;
-	}
+    /**
+     * @return the cx
+     */
+    public double getCx()
+    {
+        return cx;
+    }
 
-	/**
-	 * @param cx the cx to set
-	 */
-	public void setCx(double cx) {
-		this.cx = cx;
-	}
+    /**
+     * @param cx the cx to set
+     */
+    public void setCx(double cx)
+    {
+        this.cx = cx;
+    }
 
-	/**
-	 * @return the cy
-	 */
-	public double getCy() {
-		return cy;
-	}
+    /**
+     * @return the cy
+     */
+    public double getCy()
+    {
+        return cy;
+    }
 
-	/**
-	 * @param cy the cy to set
-	 */
-	public void setCy(double cy) {
-		this.cy = cy;
-	}
+    /**
+     * @param cy the cy to set
+     */
+    public void setCy(double cy)
+    {
+        this.cy = cy;
+    }
 
-	/**
-	 * @return the r
-	 */
-	public double getR() {
-		return r;
-	}
+    /**
+     * @return the r
+     */
+    public double getR()
+    {
+        return r;
+    }
 
-	/**
-	 * @param r the r to set
-	 */
-	public void setR(double r) {
-		this.r = r;
-	}
+    /**
+     * @param r the r to set
+     */
+    public void setR(double r)
+    {
+        this.r = r;
+    }
+
 }
