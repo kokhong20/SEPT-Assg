342800e675ad4a3886f99631a28763fc0e4d4a57
Ryne Cheow <rynec92@hotmail.com>
2013-05-07 08:54:50 +0800

    say hello

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,21 @@
+
 import gui.PARootView;
+import javax.swing.SwingUtilities;
+
 /**
  *
  * @author KokHong
  *
  */
-public class Main
-{
-    /**
-     * @param args
-     */
-    public static void main(String[] args)
-    {
-        PARootView root = new PARootView();
-        root.setVisible(true);
-    }
+public class Main {
 
+   public static void main(String[] args) {
+      SwingUtilities.invokeLater(new Runnable() {
+         @Override
+         public void run() {
+            PARootView root = new PARootView();
+            root.setVisible(true);
+         }
+      });
+   }
 }
