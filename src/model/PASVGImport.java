3458294397a768000e85d5b0894f945f013317d0
Lai Sai Hoo <laisaihoo@gmail.com>
2013-05-10 08:40:38 +0800

    regex

--- src/model/PASVGImport.java
+++ src/model/PASVGImport.java
@@ -1,8 +1,12 @@
 package model;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.LinkedList;
+import java.util.regex.Pattern;
+
+import javax.swing.JOptionPane;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -12,46 +16,78 @@ import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 /**
- *
+ * 
  * @author bryantylai/LiHao/Lai Sai Hoo
  */
 public class PASVGImport
 {
     /**
-     *
+     * 
      * Process current SVG file by parsing the SVG file to be readable in JAVA
-     *
-     * @param svgFile file from file chooser
+     * 
+     * @param svgFile
+     *            file from file chooser
      * @return svg's Document.
      */
     public static Document processFiletoDoc(File svgFile)
     {
         Document svgDoc = null;
-        DocumentBuilderFactory svgDBFactory = DocumentBuilderFactory.newInstance();
-
-        try
+        DocumentBuilderFactory svgDBFactory = DocumentBuilderFactory
+                .newInstance();
+        if (!Pattern
+                .matches(
+                        "([a-zA-Z0-9\\!\\@\\#\\$\\%\\^\\&\\(\\)\\-\\_\\=\\+\\{\\}\\[\\]\\;\\'\\,\\.\\`\\~])+(.svg|.xml)",
+                        svgFile.getName()))
         {
-            svgDBFactory.setFeature("http://xml.org/sax/features/namespaces", false);
-            svgDBFactory.setFeature("http://xml.org/sax/features/validation", false);
-            svgDBFactory.setFeature("http://apache.org/xml/features/nonvalidating/load-dtd-grammar", false);
-            svgDBFactory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+            JOptionPane.showMessageDialog(null, "The file is not a SVG file.",
+                    "Not SVG File", JOptionPane.WARNING_MESSAGE);
+            svgDoc = null;
 
-            DocumentBuilder svgDocBuilder = svgDBFactory.newDocumentBuilder();
-            svgDoc = svgDocBuilder.parse(svgFile);
-            svgDoc.getDocumentElement().normalize();
         }
-        catch (ParserConfigurationException | SAXException | IOException ex)
+        else
         {
-            System.err.println(ex.getMessage());
-        }
 
+            try
+            {
+                svgDBFactory.setFeature(
+                        "http://xml.org/sax/features/namespaces", false);
+                svgDBFactory.setFeature(
+                        "http://xml.org/sax/features/validation", false);
+                svgDBFactory
+                        .setFeature(
+                                "http://apache.org/xml/features/nonvalidating/load-dtd-grammar",
+                                false);
+                svgDBFactory
+                        .setFeature(
+                                "http://apache.org/xml/features/nonvalidating/load-external-dtd",
+                                false);
+
+                DocumentBuilder svgDocBuilder = svgDBFactory
+                        .newDocumentBuilder();
+                svgDoc = svgDocBuilder.parse(svgFile);
+                svgDoc.getDocumentElement().normalize();
+            }
+            catch (FileNotFoundException fnf)
+            {
+                JOptionPane
+                        .showMessageDialog(
+                                null,
+                                "File Not Found, Please check the file's directory is correct.",
+                                "File Not Found", JOptionPane.WARNING_MESSAGE);
+                svgDoc = null;
+            }
+            catch (ParserConfigurationException | SAXException | IOException ex)
+            {
+                System.err.println(ex.getMessage());
+            }
+        }
         return svgDoc;
     }
 
     /**
      * Read the current SVG file and add all available elements (rect, circle,
      * line, g) into a LinkedList
-     *
+     * 
      * @return a LinkedList of all shapes
      */
     public static LinkedList<PASVGElement> readSVGElements(Document svgDoc)
@@ -69,44 +105,44 @@ public class PASVGImport
             {
                 switch (drawList.item(index).getNodeName())
                 {
-                    case "rect":
-                        PARectangle newRect = new PARectangle(drawList.item(index));
-                        newRect.readAttributes();
+                case "rect":
+                    PARectangle newRect = new PARectangle(drawList.item(index));
+                    newRect.readAttributes();
 
-                        // Add new rectangle object to Shapes and put into linked
-                        // list
-                        // Rectangle2D.Double rectShape = new
-                        // Rectangle2D.Double(newRect.getX(),newRect.getY(),newRect.getWidth(),newRect.getHeight());
-                        elementCollection.add(newRect);
-                        break;
+                    // Add new rectangle object to Shapes and put into linked
+                    // list
+                    // Rectangle2D.Double rectShape = new
+                    // Rectangle2D.Double(newRect.getX(),newRect.getY(),newRect.getWidth(),newRect.getHeight());
+                    elementCollection.add(newRect);
+                    break;
 
-                    case "circle":
-                        PACircle newCircle = new PACircle(drawList.item(index));
-                        newCircle.readAttributes();
+                case "circle":
+                    PACircle newCircle = new PACircle(drawList.item(index));
+                    newCircle.readAttributes();
 
-                        // Add new circle object to Shapes and put into linked List
-                        // Ellipse2D.Double circleShape = new
-                        // Ellipse2D.Double(newCircle.getEllipse2DX(),newCircle.getEllipse2DY(),newCircle.getR()*2,newCircle.getR()*2);
+                    // Add new circle object to Shapes and put into linked List
+                    // Ellipse2D.Double circleShape = new
+                    // Ellipse2D.Double(newCircle.getEllipse2DX(),newCircle.getEllipse2DY(),newCircle.getR()*2,newCircle.getR()*2);
 
-                        elementCollection.add(newCircle);
-                        break;
+                    elementCollection.add(newCircle);
+                    break;
 
-                    case "line":
-                        PALine newLine = new PALine(drawList.item(index));
-                        newLine.readAttributes();
+                case "line":
+                    PALine newLine = new PALine(drawList.item(index));
+                    newLine.readAttributes();
 
-                        elementCollection.add(newLine);
-                        break;
+                    elementCollection.add(newLine);
+                    break;
 
-                    case "g":
-                        PASVGGroup newGroup = new PASVGGroup(drawList.item(index));
-                        newGroup.readAttributes();
+                case "g":
+                    PASVGGroup newGroup = new PASVGGroup(drawList.item(index));
+                    newGroup.readAttributes();
 
-                        elementCollection.add(newGroup);
-                        // NodeList gList = drawList.item(index).getChildNodes();
-                        // createGroups(this.elementCollection, gList,
-                        // drawList.item(index), null, null);
-                        break;
+                    elementCollection.add(newGroup);
+                    // NodeList gList = drawList.item(index).getChildNodes();
+                    // createGroups(this.elementCollection, gList,
+                    // drawList.item(index), null, null);
+                    break;
                 }
             }
         }
