2aca4a3880e2c28a055cfea784682c10dd8635de
Lai Sai Hoo <laisaihoo@gmail.com>
2013-05-13 09:05:41 +0800

    indent

--- src/controller/PATextFieldFormatter.java
+++ src/controller/PATextFieldFormatter.java
@@ -8,18 +8,24 @@ import javax.swing.JTextField;
 public class PATextFieldFormatter implements KeyListener
 {
     private JTextField textField;
-    public PATextFieldFormatter (JTextField textField)
+
+    public PATextFieldFormatter(JTextField textField)
     {
         this.textField = textField;
     }
+
     @Override
     public void keyPressed(KeyEvent ke)
     {
-        if(ke.getKeyChar()>='0' && ke.getKeyChar()<='9')
+        if (ke.getKeyChar() >= '0' && ke.getKeyChar() <= '9')
         {
             textField.setEditable(true);
         }
-        else if(ke.getKeyCode() == KeyEvent.VK_BACK_SPACE || ke.getKeyCode() == KeyEvent.VK_TAB|| ke.getKeyCode() == KeyEvent.VK_DELETE || ke.getKeyCode() == KeyEvent.VK_LEFT || ke.getKeyCode() == KeyEvent.VK_RIGHT)
+        else if (ke.getKeyCode() == KeyEvent.VK_BACK_SPACE
+                || ke.getKeyCode() == KeyEvent.VK_TAB
+                || ke.getKeyCode() == KeyEvent.VK_DELETE
+                || ke.getKeyCode() == KeyEvent.VK_LEFT
+                || ke.getKeyCode() == KeyEvent.VK_RIGHT)
         {
             textField.setEditable(true);
         }
@@ -32,35 +38,35 @@ public class PATextFieldFormatter implements KeyListener
             textField.setEditable(false);
             java.awt.Toolkit.getDefaultToolkit().beep();
         }
-        
+
     }
 
     @Override
     public void keyReleased(KeyEvent ke)
     {
         textField.setEditable(true);
-        
+
     }
 
     @Override
     public void keyTyped(KeyEvent ke)
     {
-        if(ke.getKeyChar()>='0' && ke.getKeyChar()<='9')
+        if (ke.getKeyChar() >= '0' && ke.getKeyChar() <= '9')
         {
             textField.setEditable(true);
-            
+
         }
         else if (!textField.getText().contains(".") && ke.getKeyChar() == '.')
         {
-            
+
             textField.setEditable(true);
         }
         else
         {
             textField.setEditable(false);
-            
+
         }
-        
+
     }
-    
+
 }
